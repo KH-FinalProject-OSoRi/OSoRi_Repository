866b3737be21419f958a8d88b7187bc7899ef624
jsj <a01099350974@gmail.com>
2026-01-27 14:03:01 +0900

    feat : 아이디 / 비밀번호 찾기 페이지 및 로직 구현

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,12 @@ import ProfileSettings from "./features/auth/pages/ProfileSettings";
 import AuthLayout from "./layouts/AuthLayout";
 import LoginPage from "./features/auth/pages/LoginPage";
 import RegisterPage from "./features/auth/pages/RegisterPage";
+import FindIdPage from "./features/auth/pages/FindIdPage";
+import FindPasswordPage from "./features/auth/pages/FindPasswordPage";
 import PrivateRoute from "./routes/PrivateRoute";
 import { transactions } from './Data/mockData'; //목업 수입지출데이터
 import ExpenseForm from './features/auth/pages/ExpenseForm';
-
+import ResetPasswordPage from "./features/auth/pages/ResetPasswordPage";
 
 function App() {
   const [currentDate, setCurrentDate] = useState(new Date());
@@ -21,10 +23,21 @@ function App() {
       <Routes>
         <Route path="/" element={<MainPage />} />
 
-        {/* 로그인/회원가입 화면 (헤더/푸터 없이) */}
+        {/* [BEFORE] 로그인/회원가입만 매핑 */}
+        {/*
+                <Route element={<AuthLayout />}>
+                  <Route path="/login" element={<LoginPage />} />
+                  <Route path="/register" element={<RegisterPage />} />
+                </Route>
+        */}
+
+        {/* [CHANGED] /reset-password 라우트 추가 */}
         <Route element={<AuthLayout />}>
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
+          <Route path="/find-id" element={<FindIdPage />} />
+          <Route path="/find-password" element={<FindPasswordPage />} />
+          <Route path="/reset-password" element={<ResetPasswordPage />} />
         </Route>
 
         {/* 로그인 필요 */}
@@ -38,14 +51,19 @@ function App() {
         >
           <Route index element={<MyPage />} />
           <Route path="assets" element={<MyPage />} />
-          <Route path="calendarView" element={<CalendarView
-                                                transactions={transactions}
-                                                currentDate={currentDate}
-                                                setCurrentDate={setCurrentDate}
-                                              />} />
+          <Route
+            path="calendarView"
+            element={
+              <CalendarView
+                transactions={transactions}
+                currentDate={currentDate}
+                setCurrentDate={setCurrentDate}
+              />
+            }
+          />
           <Route path="myBadges" element={<MyBadges />} />
-          <Route path="profileSettings" element={<ProfileSettings />}/>
-          <Route path='expenseForm' element={<ExpenseForm/>}/>
+          <Route path="profileSettings" element={<ProfileSettings />} />
+          <Route path="expenseForm" element={<ExpenseForm />} />
         </Route>
       </Routes>
     </Router>
@@ -53,3 +71,60 @@ function App() {
 }
 
 export default App;
+
+
+// import React, { useState } from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import MainPage from "./components/common/MainPage";
+// import MyPageLayout from "./features/auth/pages/MyPageLayout";
+// import MyPage from "./features/auth/pages/MyPage";
+// import CalendarView from "./features/menu/CalendarView";
+// import MyBadges from "./features/auth/pages/MyBadges";
+// import ProfileSettings from "./features/auth/pages/ProfileSettings";
+// import AuthLayout from "./layouts/AuthLayout";
+// import LoginPage from "./features/auth/pages/LoginPage";
+// import RegisterPage from "./features/auth/pages/RegisterPage";
+// import PrivateRoute from "./routes/PrivateRoute";
+// import { transactions } from './Data/mockData'; //목업 수입지출데이터
+// import ExpenseForm from './features/auth/pages/ExpenseForm';
+
+
+// function App() {
+//   const [currentDate, setCurrentDate] = useState(new Date());
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<MainPage />} />
+
+//         {/* 로그인/회원가입 화면 (헤더/푸터 없이) */}
+//         <Route element={<AuthLayout />}>
+//           <Route path="/login" element={<LoginPage />} />
+//           <Route path="/register" element={<RegisterPage />} />
+//         </Route>
+
+//         {/* 로그인 필요 */}
+//         <Route
+//           path="/mypage"
+//           element={
+//             <PrivateRoute>
+//               <MyPageLayout />
+//             </PrivateRoute>
+//           }
+//         >
+//           <Route index element={<MyPage />} />
+//           <Route path="assets" element={<MyPage />} />
+//           <Route path="calendarView" element={<CalendarView
+//                                                 transactions={transactions}
+//                                                 currentDate={currentDate}
+//                                                 setCurrentDate={setCurrentDate}
+//                                               />} />
+//           <Route path="myBadges" element={<MyBadges />} />
+//           <Route path="profileSettings" element={<ProfileSettings />}/>
+//           <Route path='expenseForm' element={<ExpenseForm/>}/>
+//         </Route>
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,10 +1,22 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/osori", 
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL : '/osori',
+  timeout : 10000,
+  headers : {
+    'Content-Type' : 'application/json'
+  }
 });
 
-export default api;
+export default api; 
+
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "http://localhost:8080/osori", 
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
+
+// export default api;
