7ca18fa22e7782a5d8dfcd7562cb60492b41fba4
annnerss <annaseo6375@gmail.com>
2026-03-03 14:16:32 +0900

    Fix. withCredentials 제거

--- src/api/badgeApi.js
+++ src/api/badgeApi.js
@@ -2,9 +2,7 @@ import api from "./axios";
 
 export const badgeApi = {
     getUserBadges: async (userId) => {
-        const response = await api.get(`/api/badges/${userId}`, {
-            withCredentials: true
-        });
+        const response = await api.get(`/api/badges/${userId}`);
         return response.data;
     },
 
