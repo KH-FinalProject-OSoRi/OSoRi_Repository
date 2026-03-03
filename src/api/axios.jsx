3ab15119fdfc5606c525977414e046417a1d7a0f
annnerss <annaseo6375@gmail.com>
2026-03-03 12:14:04 +0900

    Fix. 경로 수정

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL : '/osori',
+    baseURL : 'http://13.239.33.140:8080',
     timeout : 10000,
     headers : {
         'Content-Type' : 'application/json'
